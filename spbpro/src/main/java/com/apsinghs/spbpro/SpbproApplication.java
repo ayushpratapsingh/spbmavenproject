0f993df81dcac8a241cdbfe4cc9212d3e673a157
ayushpratapsingh <ayush.pratap906@gmail.com>
2025-07-05 17:33:56 +0530

    spring boot project addition

---
+++ spbpro/src/main/java/com/apsinghs/spbpro/SpbproApplication.java
@@ -0,0 +1,20 @@
+package com.apsinghs.spbpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RequestMapping("/spring-boot")
+@RestController
+public class SpbproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbproApplication.class, args);
+	}
+	@GetMapping("/hello")
+       public String firstApp(@RequestParam(value = "name", defaultValue = "Coder") String name) 
+	   {
+       return String.format("Congratulations  %s, you've built your first Spring Boot Application from Scratch!!", name);
+       }
+
+}
---
+++ spbpro/src/test/java/com/apsinghs/spbpro/SpbproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apsinghs.spbpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
