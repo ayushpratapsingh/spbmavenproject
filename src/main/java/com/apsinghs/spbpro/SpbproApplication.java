4a0e16831512146346f72dc1cc1a7b35397bfae2
ayushpratapsingh <ayush.pratap906@gmail.com>
2025-07-10 21:12:10 +0530

    Add import statements SpbproApplication.java
    import org.springframework.boot.SpringApplication;
    import org.springframework.boot.autoconfigure.SpringBootApplication;
    import org.springframework.web.bind.annotation.RequestMapping;
    import org.springframework.web.bind.annotation.RestController;
    import org.springframework.web.bind.annotation.RequestParam;
    import org.springframework.web.bind.annotation.GetMapping;

--- src/main/java/com/apsinghs/spbpro/SpbproApplication.java
+++ src/main/java/com/apsinghs/spbpro/SpbproApplication.java
@@ -2,6 +2,10 @@ package com.apsinghs.spbpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @RequestMapping("/spring-boot")
